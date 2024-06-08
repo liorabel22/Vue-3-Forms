03623b2f95eb888ba7493da74c3540ce0cdb15ed
liorabel22 <60816560+liorabel22@users.noreply.github.com>
2024-06-08 23:31:56 +0300

    done

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <label>{{ label }}</label>
+  <input
+    v-bind="$attrs"
+    :placeholder="label"
+    class="field"
+    :value="modelValue"
+    @input="onInput"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+  setup(props, { emit }) {
+    const onInput = (e) => {
+      emit("update:modelValue", e.target.value);
+    };
+
+    return {
+      onInput,
+    };
+  },
+};
+</script>
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>Create an event</h1>
     <form>
-
       <label>Select a category</label>
       <select v-model="event.category">
         <option
@@ -10,74 +9,40 @@
           :value="option"
           :key="option"
           :selected="option === event.category"
-        >{{ option }}</option>
+        >
+          {{ option }}
+        </option>
       </select>
 
       <h3>Name & describe your event</h3>
 
-      <label>Title</label>
-      <input
-        v-model="event.title"
-        type="text"
-        placeholder="Title"
-        class="field"
-      >
+      <BaseInput v-model="event.title" label="Title" type="text" />
 
-      <label>Description</label>
-      <input
-        v-model="event.description"
-        type="text"
-        placeholder="Description"
-        class="field"
-      />
+      <BaseInput v-model="event.description" label="Description" type="text" />
 
       <h3>Where is your event?</h3>
 
-      <label>Location</label>
-      <input
-        v-model="event.location"
-        type="text"
-        placeholder="Location"
-        class="field"
-      />
+      <BaseInput v-model="event.location" label="Location" type="text" />
 
       <h3>Are pets allowed?</h3>
       <div>
-        <input
-            type="radio"
-            v-model="event.pets"
-            :value="1"
-            name="pets"
-          />
+        <input type="radio" v-model="event.pets" :value="1" name="pets" />
         <label>Yes</label>
       </div>
 
       <div>
-        <input
-          type="radio"
-          v-model="event.pets"
-          :value="0"
-          name="pets"
-        />
+        <input type="radio" v-model="event.pets" :value="0" name="pets" />
         <label>No</label>
       </div>
 
       <h3>Extras</h3>
       <div>
-        <input
-          type="checkbox"
-          v-model="event.extras.catering"
-          class="field"
-        />
+        <input type="checkbox" v-model="event.extras.catering" class="field" />
         <label>Catering</label>
       </div>
 
       <div>
-        <input
-          type="checkbox"
-          v-model="event.extras.music"
-          class="field"
-        />
+        <input type="checkbox" v-model="event.extras.music" class="field" />
         <label>Live music</label>
       </div>
 
@@ -87,30 +52,34 @@
 </template>
 
 <script>
+import BaseInput from "@/components/BaseInput.vue";
 export default {
-  data () {
+  components: {
+    BaseInput,
+  },
+  data() {
     return {
       categories: [
-        'sustainability',
-        'nature',
-        'animal welfare',
-        'housing',
-        'education',
-        'food',
-        'community'
+        "sustainability",
+        "nature",
+        "animal welfare",
+        "housing",
+        "education",
+        "food",
+        "community",
       ],
       event: {
-        category: '',
-        title: '',
-        description: '',
-        location: '',
+        category: "",
+        title: "",
+        description: "",
+        location: "",
         pets: 1,
         extras: {
           catering: false,
-          music: false
-        }
-      }
-    }
-  }
-}
+          music: false,
+        },
+      },
+    };
+  },
+};
 </script>
