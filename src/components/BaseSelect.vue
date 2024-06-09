09068ff9e585f03749f865c0f27655256c5d7855
liorabel22 <60816560+liorabel22@users.noreply.github.com>
2024-06-09 05:49:23 +0300

    done

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,50 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select
+    :value="modelValue"
+    class="field"
+    v-bind="{
+      ...$attrs,
+      onChange: ($event) => {
+        $emit('update:modelValue', $event.target.value);
+      },
+    }"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props, { emit }) {
+    const onChange = (event) => {
+      emit("update:modelValue", event.target.value);
+    };
+
+    return {
+      onChange,
+    };
+  },
+};
+</script>
